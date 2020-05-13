d7729c9631c4d0d2c75d64b3eac39a13b70414f5
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-05-13 11:59:10 +0100

    Better job of updating .env

--- application/libraries/Env.php
+++ application/libraries/Env.php
@@ -9,7 +9,16 @@ class Env
 {
     public function __construct()
     {
-        $dotenv = Dotenv\Dotenv::create(APPPATH . 'environment');
-        $dotenv->load();
+        //Amezmo deployment
+        if(file_exists('/webroot/.env')):
+            copy('/webroot/.env', APPPATH . 'environment/.env');
+        endif;
+
+        //Only initialize this library if .env exists
+        if(file_exists(APPPATH . 'environment/.env')):
+            $dotenv = Dotenv\Dotenv::create(APPPATH . 'environment');
+            $dotenv->load();
+        endif;
+
     }
 }
