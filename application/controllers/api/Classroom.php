ea6ac0960ce5994912b139c9e84b976f484798aa
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-05-13 12:19:21 +0100

    Updated controller

--- application/controllers/api/Classroom.php
+++ application/controllers/api/Classroom.php
@@ -25,11 +25,8 @@ class Classroom extends REST_Controller {
 
         $apiKey = $this->uri->segment(2);
 
-<<<<<<< HEAD
         echo env('MY_API_KEY');
-=======
         echo $apiKey;
->>>>>>> fba2f4e73320cde4c2b16cbd5017bbaed494ff54
         die();
 
         if (env('MY_API_KEY') !== $apiKey ):
