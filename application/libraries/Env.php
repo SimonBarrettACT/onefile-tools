7a91e3f9e3f3a15c90e74bb950a69166ae33b7a5
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-05-13 11:46:40 +0100

    Copy .env to the correct folder

--- application/libraries/Env.php
+++ application/libraries/Env.php
@@ -9,19 +9,7 @@ class Env
 {
     public function __construct()
     {
-        //Only load this library if .env exists
-
-        //Normal deployment
-        if(file_exists(APPPATH . 'environment/.env')):
-            $dotenv = Dotenv\Dotenv::create(APPPATH . 'environment');
-            $dotenv->load();
-        endif;
-
-        //Amezmo deployment
-        if(file_exists('/webroot/environment')):
-            $dotenv = Dotenv\Dotenv::create('/webroot/environment');
-            $dotenv->load();
-        endif;
-
+        $dotenv = Dotenv\Dotenv::create(APPPATH . 'environment');
+        $dotenv->load();
     }
 }
